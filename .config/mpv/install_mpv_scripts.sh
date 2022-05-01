1264ad80659d3797ae65697e608d316bec23f770
Muhammad Faizud Daroin <mfaizudd@gmail.com>
2022-05-01 20:22:50 +0700

    fix: use https instead of ssh

--- .config/mpv/install_mpv_scripts.sh
+++ .config/mpv/install_mpv_scripts.sh
@@ -3,7 +3,7 @@
 MPV_CONFIG=~/.config/mpv/scripts
 
 mkdir -p $MPV_CONFIG
-git clone git@github.com:jonniek/mpv-nextfile.git $MPV_CONFIG/mpv-nextfile
-git clone git@github.com:hacel/recent $MPV_CONFIG/recent
+git clone https://github.com/jonniek/mpv-nextfile.git $MPV_CONFIG/mpv-nextfile
+git clone https://github.com/hacel/recent $MPV_CONFIG/recent
 ln -s $MPV_CONFIG/mpv-nextfile/nextfile.lua $MPV_CONFIG/nextfile.lua
 ln -s $MPV_CONFIG/recent/recent.lua $MPV_CONFIG/recent.lua
