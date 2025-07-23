6f8339d79ab7997916a50eecbe0ab41d41685d29
Muhammad Faizud Daroin <m@faizud.net>
2025-07-23 08:34:29 +0700

    fix: rofi power

--- dot_config/scripts/executable_power.sh
+++ dot_config/scripts/executable_power.sh
@@ -1,16 +1,3 @@
 #!/usr/bin/env bash
 
-entries="⇠ Logout\n⏾ Suspend\n⭮ Reboot\n⏻ Shutdown"
-
-selected=$(echo -e $entries|wofi -i --width 250 --height 210 --dmenu --cache-file /dev/null | awk '{print tolower($2)}')
-
-case $selected in
-  logout)
-    hyprctl dispatch exit;;
-  suspend)
-    exec systemctl suspend;;
-  reboot)
-    exec systemctl reboot;;
-  shutdown)
-    exec systemctl poweroff -i;;
-esac
+rofi -show power-menu -modi power-menu:rofi-power-menu
