d9f0f71fd4f3ffa23fdf5c0dfe703330b625ada2
Muhammad Faizud Daroin <m@faizud.net>
2026-08-08 08:50:52 +0700

    add opam

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -65,3 +65,12 @@ end
 set --erase _asdf_shims
 
 set -q GHCUP_INSTALL_BASE_PREFIX[1]; or set GHCUP_INSTALL_BASE_PREFIX $HOME ; set -gx PATH $HOME/.cabal/bin $PATH /home/bandeng/.ghcup/bin # ghcup-env
+
+
+# BEGIN opam configuration
+# This is useful if you're using opam as it adds:
+#   - the correct directories to the PATH
+#   - auto-completion for the opam binary
+# This section can be safely removed at any time if needed.
+test -r '/home/bandeng/.opam/opam-init/init.fish' && source '/home/bandeng/.opam/opam-init/init.fish' > /dev/null 2> /dev/null; or true
+# END opam configuration
