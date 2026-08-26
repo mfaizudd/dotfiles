7ee4276390f32aa5b505805c9fe18b2a24cbff07
Muhammad Faizud Daroin <m@faizud.net>
2026-08-26 08:39:30 +0700

    Godot zellij

--- private_dot_local/bin/executable_godot-nvim.sh
+++ private_dot_local/bin/executable_godot-nvim.sh
@@ -4,7 +4,7 @@ FILE="$1"
 LINE="$2"
 COL="$3"
 NVIM_SOCKET="/tmp/nvim-godot.sock"
-ALACRITTY_WORKSPACE=2
+ALACRITTY_WORKSPACE=5
 
 if [ -S "$NVIM_SOCKET" ]; then
     # Connect to running instance
@@ -14,16 +14,20 @@ if [ -S "$NVIM_SOCKET" ]; then
     # Switch to the nvim workspace if on Hyprland
     if [ -n "$HYPRLAND_INSTANCE_SIGNATURE" ]; then
         hyprctl dispatch workspace "$ALACRITTY_WORKSPACE"
+    elif [ -n "$NIRI_SOCKET" ]; then
+        niri msg action focus-workspace 2
     fi
-elif [ -n "$TMUX" ]; then
+elif [ -n "$ZELLIJ" ]; then
     nvim --listen "$NVIM_SOCKET"
 else
     # No socket — open alacritty with tmux, running nvim
-    alacritty -e tmux new-session \
+    alacritty -e zellij \
         "nvim --listen '$NVIM_SOCKET' +'$LINE' '$FILE'" &
 
     # Switch to it if on Hyprland
     if [ -n "$HYPRLAND_INSTANCE_SIGNATURE" ]; then
         hyprctl dispatch workspace "$ALACRITTY_WORKSPACE"
+    elif [ -n "$NIRI_SOCKET" ]; then
+        niri msg action focus-workspace 2
     fi
 fi
